4864f59121afc38a84fc42ac9f024f4e0f33805a
Guilherme Rocha <dev-guirocha@MacBook-Air-de-Guilherme.local>
2025-12-19 16:35:26 -0300

    Adiciona localhost:5174 na lista padrao de CORS

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ app.set('trust proxy', 1);
 const defaultOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:3000',
+  'http://127.0.0.1:5174',
   'https://www.pandaloterias.com',
   'https://pandaloterias.com',
 ];
